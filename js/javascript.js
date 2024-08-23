a0f4937a8e79279f3048c3d8508ca7a272f09cdf
Briarivero <marcosbrayan03@gmail.com>
2024-08-23 17:07:53 +0200

    .

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+const typewriterText = document.querySelector(".type");
+const phrases = [
+  "© paradise.pw 2024",
+  "one step beyond"
+];
+
+let phraseIndex = 0;
+let characterIndex = 0;
+
+function type() {
+  if (characterIndex < phrases[phraseIndex].length) {
+    typewriterText.textContent += phrases[phraseIndex][characterIndex];
+    characterIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1000);
+  }
+}
+
+function erase() {
+  if (characterIndex > 0) {
+    typewriterText.textContent = phrases[phraseIndex].substring(0, characterIndex - 1);
+    characterIndex--;
+    setTimeout(erase, 50);
+  } else {
+    phraseIndex = (phraseIndex + 1) % phrases.length;
+    setTimeout(type, 250);
+  }
+}
+
+type();
